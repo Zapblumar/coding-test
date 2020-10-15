0988d7445d153d1211f69d99db9d839e93b95051
Tylor <Zapblumar@gmail.com>
2020-10-15 15:09:43 -0700

    got the timer working

--- assets/css/test.js
+++ assets/css/test.js
@@ -4,6 +4,19 @@
 
 var startBtn = document.querySelector("#start");
 var quiz = document.querySelector("main");
+var passwordInput = document.querySelector('#high-score');
+var timeLimit = document.getElementById('countdown');
+
+
+
+function renderLastRegistered() {
+    var score = localStorage.getItem('high-score');
+  
+    if ( score === null) {
+      return;
+    }
+}
+
 
 var start = function () {
  quiz.innerHTML = "";
@@ -20,10 +33,27 @@ questions = [
 
 
 
+function countdown() {
+    var timeLeft = 30;
+
+    var timeInterval = setInterval(function() {
+      if (timeLeft > -1) {
+        timeLimit.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 0) {
+        timeLimit.textContent = timeLeft + ' second remaining';
+        timeLeft--;
+      }
+    }, 1000);
+  }
 
 
 
 
 
 
+
+ // localStorage.setItem('high-score', score);
+
+ startBtn.onclick = countdown;
 startBtn.addEventListener("click", start);
