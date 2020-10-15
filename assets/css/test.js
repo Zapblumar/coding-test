78a3d3fddff9022731fc573efe8abfb306b4145d
Tylor <Zapblumar@gmail.com>
2020-10-15 13:00:43 -0700

    got button to work

---
+++ assets/css/test.js
@@ -0,0 +1,29 @@
+
+
+
+
+var startBtn = document.querySelector("#start");
+var quiz = document.querySelector("main");
+
+var start = function () {
+ quiz.innerHTML = "";
+
+}
+
+questions = [
+    {question: "bla bla bla"},
+    {question: "bla bla bla"},
+    {question: "bla bla bla"},
+    {question: "bla bla bla"},
+    {question: "bla bla bla"},
+];
+
+
+
+
+
+
+
+
+
+startBtn.addEventListener("click", start);
