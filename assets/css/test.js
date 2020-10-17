293fa4701384d38403a0d1954570dc8e2b8564ef
Tylor <Zapblumar@gmail.com>
2020-10-17 12:59:36 -0700

    got questions working

--- assets/css/test.js
+++ assets/css/test.js
@@ -17,9 +17,7 @@ var correct = document.querySelector("answer")
 function renderLastRegistered() {
     var score = localStorage.getItem('high-score');
   
-    if ( score === null) {
-      return;
-    }
+    
 }
 
 
@@ -52,10 +50,25 @@ buttonD.innerText = questions[0].answerD;
 
 quiz.appendChild(buttonD);
 
+
+buttonA.onclick = correctAnswer;
+buttonB.onclick = wrongAnswer;
+buttonC.onclick = wrongAnswer;
+buttonD.onclick = wrongAnswer;
+
+var feedback = document.querySelector("#feedback")
+function correctAnswer(){
+ feedback.textContent = "correct";
+  next()
 }
-// if("bla bla bla === A")
-debugger;
+
+function wrongAnswer(){
+  feedback.textContent = "wrong";
+  next()
+}
+
 function next(){
+  quiz.innerHTML = "";
  sample = document.createElement("h3");
 sample.innerText = questions[1].question
 
@@ -78,6 +91,155 @@ buttonD.innerText = questions[1].answerD;
 
 quiz.appendChild(buttonD);
 
+buttonA.onclick = correctAnswer;
+buttonB.onclick = wrongAnswer;
+buttonC.onclick = wrongAnswer;
+buttonD.onclick = wrongAnswer;
+
+var feedback = document.querySelector("#feedback")
+function correctAnswer(){
+ feedback.textContent = "correct";
+  next2()
+}
+
+function wrongAnswer(){
+  feedback.textContent = "wrong";
+  next2()
+}
+}
+function next2(){
+  quiz.innerHTML = "";
+ sample = document.createElement("h3");
+sample.innerText = questions[2].question
+
+quiz.appendChild(sample);
+
+ buttonA = document.createElement("button");
+buttonA.innerText = questions[2].answerA;
+
+quiz.appendChild(buttonA);
+ buttonB = document.createElement("button");
+buttonB.innerText = questions[2].answerB;
+
+quiz.appendChild(buttonB);
+ buttonC = document.createElement("button");
+buttonC.innerText = questions[2].answerC;
+
+quiz.appendChild(buttonC);
+ buttonD = document.createElement("button");
+buttonD.innerText = questions[2].answerD;
+
+quiz.appendChild(buttonD);
+
+buttonA.onclick = correctAnswer;
+buttonB.onclick = wrongAnswer;
+buttonC.onclick = wrongAnswer;
+buttonD.onclick = wrongAnswer;
+
+var feedback = document.querySelector("#feedback")
+function correctAnswer(){
+ feedback.textContent = "correct";
+  next3()
+}
+
+function wrongAnswer(){
+  feedback.textContent = "wrong";
+  next3()
+}
+}
+
+//function correctAnswer(){
+  //window.alert("correct");
+  }
+  
+  function wrongAnswer(){
+    //window.alert("wrong");
+    }
+function next3(){
+  quiz.innerHTML = "";
+ sample = document.createElement("h3");
+sample.innerText = questions[3].question
+
+quiz.appendChild(sample);
+
+ buttonA = document.createElement("button");
+buttonA.innerText = questions[3].answerA;
+
+quiz.appendChild(buttonA);
+ buttonB = document.createElement("button");
+buttonB.innerText = questions[3].answerB;
+
+quiz.appendChild(buttonB);
+ buttonC = document.createElement("button");
+buttonC.innerText = questions[3].answerC;
+
+quiz.appendChild(buttonC);
+ buttonD = document.createElement("button");
+buttonD.innerText = questions[3].answerD;
+
+quiz.appendChild(buttonD);
+
+buttonA.onclick = correctAnswer;
+buttonB.onclick = wrongAnswer;
+buttonC.onclick = wrongAnswer;
+buttonD.onclick = wrongAnswer;
+
+var feedback = document.querySelector("#feedback")
+function correctAnswer(){
+ feedback.textContent = "correct";
+  next4()
+}
+
+function wrongAnswer(){
+  feedback.textContent = "wrong";
+  next4()
+}
+}
+
+function correctAnswer(){
+  //window.alert("correct");
+  }
+  
+  function wrongAnswer(){
+   // window.alert("wrong");
+    }
+function next4(){
+  quiz.innerHTML = "";
+ sample = document.createElement("h3");
+sample.innerText = questions[4].question
+
+quiz.appendChild(sample);
+
+ buttonA = document.createElement("button");
+buttonA.innerText = questions[4].answerA;
+
+quiz.appendChild(buttonA);
+ buttonB = document.createElement("button");
+buttonB.innerText = questions[4].answerB;
+
+quiz.appendChild(buttonB);
+ buttonC = document.createElement("button");
+buttonC.innerText = questions[4].answerC;
+
+quiz.appendChild(buttonC);
+ buttonD = document.createElement("button");
+buttonD.innerText = questions[4].answerD;
+
+quiz.appendChild(buttonD);
+
+buttonA.onclick = correctAnswer;
+buttonB.onclick = wrongAnswer;
+buttonC.onclick = wrongAnswer;
+buttonD.onclick = wrongAnswer;
+
+var feedback = document.querySelector("#feedback")
+function correctAnswer(){
+ feedback.textContent = "correct";
+}
+
+function wrongAnswer(){
+  feedback.textContent = "wrong";
+}
 }
 
 
