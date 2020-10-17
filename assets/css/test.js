6b6146ad128a9d413680e373f2b193226d486c08
Tylor <Zapblumar@gmail.com>
2020-10-16 17:18:52 -0700

    got question1 and buttons up

--- assets/css/test.js
+++ assets/css/test.js
@@ -4,8 +4,13 @@
 
 var startBtn = document.querySelector("#start");
 var quiz = document.querySelector("main");
-var passwordInput = document.querySelector('#high-score');
-var timeLimit = document.getElementById('countdown');
+var passwordInput = document.querySelector("#high-score");
+var timeLimit = document.getElementById("countdown");
+var correct = document.querySelector("answer")
+
+
+
+
 
 
 
@@ -21,16 +26,59 @@ function renderLastRegistered() {
 var start = function () {
  quiz.innerHTML = "";
 
+
+
+
+
+var sample = document.createElement("h3");
+sample.innerText = questions[0].question
+
+quiz.appendChild(sample);
+
+var buttonA = document.createElement("button");
+buttonA.innerText = questions[0].answerA;
+
+quiz.appendChild(buttonA);
+var buttonB = document.createElement("button");
+buttonB.innerText = questions[0].answerB;
+
+quiz.appendChild(buttonB);
+var buttonC = document.createElement("button");
+buttonC.innerText = questions[0].answerC;
+
+quiz.appendChild(buttonC);
+var buttonD = document.createElement("button");
+buttonD.innerText = questions[0].answerD;
+
+quiz.appendChild(buttonD);
+
 }
+// if("bla bla bla === A")
+debugger;
+function next(){
+ sample = document.createElement("h3");
+sample.innerText = questions[1].question
 
-questions = [
-    {question: "bla bla bla"},
-    {question: "bla bla bla"},
-    {question: "bla bla bla"},
-    {question: "bla bla bla"},
-    {question: "bla bla bla"},
-];
+quiz.appendChild(sample);
 
+ buttonA = document.createElement("button");
+buttonA.innerText = questions[1].answerA;
+
+quiz.appendChild(buttonA);
+ buttonB = document.createElement("button");
+buttonB.innerText = questions[1].answerB;
+
+quiz.appendChild(buttonB);
+ buttonC = document.createElement("button");
+buttonC.innerText = questions[1].answerC;
+
+quiz.appendChild(buttonC);
+ buttonD = document.createElement("button");
+buttonD.innerText = questions[1].answerD;
+
+quiz.appendChild(buttonD);
+
+}
 
 
 function countdown() {
@@ -38,10 +86,10 @@ function countdown() {
 
     var timeInterval = setInterval(function() {
       if (timeLeft > -1) {
-        timeLimit.textContent = timeLeft + ' seconds remaining';
+        timeLimit.textContent = timeLeft + "seconds remaining";
         timeLeft--;
       } else if (timeLeft === 0) {
-        timeLimit.textContent = timeLeft + ' second remaining';
+        timeLimit.textContent = timeLeft + "second remaining";
         timeLeft--;
       }
     }, 1000);
@@ -57,3 +105,12 @@ function countdown() {
 
  startBtn.onclick = countdown;
 startBtn.addEventListener("click", start);
+
+
+var questions = [
+  {question: "bla bla bla", answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+  {question: "bla blu blu", answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+  {question: "bla ble ble", answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+  {question: "bla blr blr", answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+  {question: "bla bli bli", answerA:"A",answerB:"B",answerC:"C",answerD:"D"},
+];
